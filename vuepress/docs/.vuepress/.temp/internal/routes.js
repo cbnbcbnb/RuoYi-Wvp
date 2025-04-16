726036a7159be0bc107806c7acdec39001ce3e22
fengcheng <2929004360@qq.com>
2025-04-16 16:29:49 +0800

    添加文档

--- vuepress/docs/.vuepress/.temp/internal/routes.js
+++ vuepress/docs/.vuepress/.temp/internal/routes.js
@@ -2,8 +2,6 @@ export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
   ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"C:/Users/17988/Desktop/ruoyi/RuoYi-Wvp/vuepress/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Get Started"} }],
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"C:/Users/17988/Desktop/ruoyi/RuoYi-Wvp/vuepress/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"首页"} }],
-  ["/doc/", { loader: () => import(/* webpackChunkName: "doc_index.html" */"C:/Users/17988/Desktop/ruoyi/RuoYi-Wvp/vuepress/docs/.vuepress/.temp/pages/doc/index.html.js"), meta: {"title":"介绍"} }],
   ["/doc/kslj.html", { loader: () => import(/* webpackChunkName: "doc_kslj.html" */"C:/Users/17988/Desktop/ruoyi/RuoYi-Wvp/vuepress/docs/.vuepress/.temp/pages/doc/kslj.html.js"), meta: {"title":"1"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"C:/Users/17988/Desktop/ruoyi/RuoYi-Wvp/vuepress/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
@@ -15,6 +13,16 @@ if (import.meta.webpackHot) {
   }
   if (__VUE_HMR_RUNTIME__.updateRedirects) {
     __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
+ts(redirects)
   }
 }
 
